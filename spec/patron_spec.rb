1c65bbffc26d0571d71e60c468ef5ddc16030ea0
Weston Sandfort <sandfortw@gmail.com>
2023-01-03 09:14:21 -0700

    Create patron class to finish iteration 1

--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -3,22 +3,22 @@ require './lib/patron.rb'
 describe Patron do
 
   before(:each) do 
-    patron_1 = Patron.new("Bob", 20)
+    @patron_1 = Patron.new("Bob", 20)
   end
 
   it 'exists' do
-    expect(patron_1).to be_a(Patron)
+    expect(@patron_1).to be_a(Patron)
   end
 
   it 'has a name, spending money, and interests' do
-    expect(patron_1.name).to eq 'Bob'
-    expect(patron_1.interests).to eq []
-    expect(patron_1.spending_money).to eq 20
+    expect(@patron_1.name).to eq 'Bob'
+    expect(@patron_1.interests).to eq []
+    expect(@patron_1.spending_money).to eq 20
   end
 
   it 'can add interest' do
-    patron_1.add_interest("Dead Sea Scrolls")
-    patron_1.add_interest("Gems and Minerals")
-    expect(patron_1.interests).to eq(["Dead Sea Scrolls", "Gems and Minerals"])
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_1.add_interest("Gems and Minerals")
+    expect(@patron_1.interests).to eq(["Dead Sea Scrolls", "Gems and Minerals"])
   end
 end
