3d19ac804358b24b7e90b85befc8d4575a6e7ed5
Weston Sandfort <sandfortw@gmail.com>
2023-01-03 09:04:21 -0700

    Add tests for exhibit

--- spec/exhibit_spec.rb
+++ spec/exhibit_spec.rb
@@ -1,6 +1,16 @@
-require 'rspec'
 require './lib/exhibit'
 
 RSpec.describe Exhibit do
+
+  before(:each) do 
+    @exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+  end
+  it 'exists' do
+    expect(@exhibit).to be_an(Exhibit)
+  end
   
+  it 'has attributes' do
+    expect(@exhibit.name).to eq("Gems and Minerals")
+    expect(@exhibit.cost).to eq 0
+  end
 end
